8e72413e639aeb42c65a5d3540ef6bbd299936e0
Lyubimov Roman <lubimov.nsk@gmail.com>
2023-04-13 12:02:01 +0300

    Adds SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     MetrikaModule.forRoot({
       id: 45631461,
       webvisor: true,
